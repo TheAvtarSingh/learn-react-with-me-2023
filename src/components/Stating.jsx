241bae36921967c9c8a4e87a7d358d64ca521da1
AVTAR SINGH <avtarsingh@AVTARs-MacBook-Air.local>
2023-11-16 16:42:57 +0530

    ReactFibreandReconcillation

--- src/components/Stating.jsx
+++ src/components/Stating.jsx
@@ -2,43 +2,45 @@ import React, { useState } from 'react'
 
 function Stating() {
 
-    const[isRed, setRed] = useState(false);
+  const [isRed, setRed] = useState(false);
 
-    // Using Callback function to get the previous state
+  // Using Callback function to get the previous state
 
-    const[count, addCount] = useState(0);
+  const [count, addCount] = useState(0);
 
-    /*
-    const updateCount = () => {
-      addCount((prevCount) => {
-        console.log('previous count : ',prevCount);
-        return prevCount + 1;
-      });
-    };
+  /*
+  const updateCount = () => {
+    addCount((prevCount) => {
+      console.log('previous count : ',prevCount);
+      return prevCount + 1;
+    });
+  };
 
-    */
+  */
 
 
-    // useState is async
+  // useState is async
 
-    const updateCount = () => {
-      console.log('before function call',count);
-      addCount((prevCount) => {
-        console.log('previous count : ',prevCount);
-        return prevCount + 1;
-      });
-      console.log('after function call : ',count);
-    };
-    
+  const updateCount = () => {
+    console.log('before function call', count);
+    addCount((prevCount) => {
+      console.log('previous count : ', prevCount);
+      return prevCount + 1;
+    });
+    console.log('after function call : ', count);
+  };
+
+   
+
 
   return (
     <div>
-        <h1>Change my color</h1>
-        {isRed && <h3>Color is Red</h3>}
-        <button onClick={() => setRed(!isRed)}>Change color</button>
-        <h1>Add Count</h1>
-        count: {count}
-        <button onClick={updateCount}>Add Count</button>
+      <h1>Change my color</h1>
+      {isRed && <h3>Color is Red</h3>}
+      <button onClick={() => setRed(!isRed)}>Change color</button>
+      <h1>Add Count</h1>
+      count: {count}
+      <button onClick={updateCount}>Add Count</button>
     </div>
   )
 }
